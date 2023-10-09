ea6c6d240f6122459b4ba39d5cf5fa924a982bb6
Filip Strajnar <filip.strajnar@gmail.com>
2023-10-09 22:45:33 +0200

    Some work done on WeatherService.

--- backend/WeatherService.cs
+++ backend/WeatherService.cs
@@ -1,11 +1,22 @@
+using System.Security.Authentication;
+
 namespace backend;
 
 public class WeatherService
 {
-    private readonly Dictionary<(double Latitude, double Longitude), WeatherData> cachedData;
+    private readonly string _openWeatherMapKey;
+    private readonly Dictionary<(double Latitude, double Longitude), WeatherData> _cachedData;
 
     public WeatherService()
     {
-        this.cachedData = new();
+        this._cachedData = new();
+        if (Environment.GetEnvironmentVariable("OPENWEATHERMAP_KEY") is string key)
+        {
+            this._openWeatherMapKey = key;
+        }
+        else
+        {
+            throw new InvalidCredentialException("API key for Open Weather Map is not defined.");
+        }
     }
 }
