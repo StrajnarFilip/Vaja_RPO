e4043ef6a3e5c5ffbc0f9def5da54a3f8f6a9e43
Filip Strajnar <filip.strajnar@gmail.com>
2023-10-17 21:38:27 +0200

    Added WeatherForecastEntity.

---
+++ backend/Entities/WeatherForecastEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Entities
+{
+    public class WeatherForecastEntity
+    {
+        public double HighestDailyTemperature { get; set; }
+        public double LowestDailyTemperature { get; set; }
+        public double AveragePrecipitationProbability { get; set; }
+        public double AverageWindSpeed { get; set; }
+    }
+}
